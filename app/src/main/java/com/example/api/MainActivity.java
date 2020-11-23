6ff1798b14d9407888f29565f26e316859626950
Yuzux <fuchengly@gmail.com>
2020-11-23 17:12:10 +0100

    +Recherche micro update

--- app/src/main/java/com/example/api/MainActivity.java
+++ app/src/main/java/com/example/api/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -50,6 +51,36 @@ public class MainActivity extends AppCompatActivity {
                 jsonParse();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("DEVE0304", "MainActivity:onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("DEVE0304", "MainActivity:onStop()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("DEVE0304", "MainActivity:onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("DEVE0304", "MainActivity:onPause()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("DEVE0304", "MainActivity:onDestroy()");
     }
 
     private void jsonParse() {
